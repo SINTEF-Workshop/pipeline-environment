d0a27bfc4208d39420bba6cccd5084d28bf3e5fe
erikjny <44464764+erikjny@users.noreply.github.com>
2022-10-06 13:35:00 +0000

    Updated dagster_pipeline

--- test_dagster/test_dagster/jobs/dump_to_s3.py
+++ test_dagster/test_dagster/jobs/dump_to_s3.py
@@ -1,7 +1,7 @@
 from dagster import job, fs_io_manager, mem_io_manager, op
 # from dagster.core.definitions.executor_definition import execute_in_process_executor, executor
 
-from test_dagster.ops.my_ops import collect_data, create_dump_file, dump_file
+from test_dagster.ops.my_ops import create_dump_file, dump_file, get_interval, collect_messages, save_to_parquet
 # from test_dagster.ops.create_dump_file import create_dump_file
 # from test_dagster.ops.consume import dump_file, collect_data
 # from test_dagster.resources.connection_resources import minio_client, nats_client
@@ -21,7 +21,10 @@ def dump_to_s3():
     """
     Collects AIS-data and dumps it to s3
     """
-    time = create_dump_file()
-    file_name = collect_data(time)
-    dump_file(file_name)
+    interval = get_interval()
+    # time = create_dump_file()
+    messages = collect_messages(interval)
+    file_path = save_to_parquet(interval, messages)
+    # file_name = collect_data(interval)
+    # dump_file(file_name)
     
--- test_dagster/test_dagster/ops/my_ops.py
+++ test_dagster/test_dagster/ops/my_ops.py
@@ -1,32 +1,65 @@
-from dagster import Out, get_dagster_logger, op
+from dagster import get_dagster_logger, op
+import os
 import json
-
+import pytz
+import pandas as pd
 import dateutil.parser
+from pendulum import date
+from datetime import datetime, timedelta
 
 @op(required_resource_keys={"nats_client"})
-async def collect_data(context, create_dump_file):
+async def collect_messages(context, create_dump_file) -> pd.DataFrame:
+    """ Collect messages from nats stream and store in DataFrame"""
     nats_conn = context.resources.nats_client
-    """ An op definition. This example op outputs a single string. """
+    end_time, start_time = create_dump_file
+
     # Connect to local nats server
     nc = await nats_conn.get_connection("localhost:4222")
-
-    # Jetstream
     js = nc.jetstream()
 
-    # Pull based consumer on 'ais'.
+    # Subscribe to the stream
     psub = await js.subscribe("ais", durable="fff")
+    
+    # Collect messages to DataFrame
+    messages_df = await get_messages(start_time, end_time, psub)
 
-    end_time = create_dump_file[0]
-    start_time = create_dump_file[1]
-
-    await get_messages(start_time, end_time, psub)
-
-    file = f"dagster_ais/data/{format(str(end_time))}"
     # Close NATS connection
     await nc.close()
 
-    return file
+    return messages_df
 
+# @op(required_resource_keys={"nats_client"})
+# async def collect_data(context, create_dump_file):
+#     nats_conn = context.resources.nats_client
+#     """ An op definition. This example op outputs a single string. """
+#     # Connect to local nats server
+#     nc = await nats_conn.get_connection("localhost:4222")
+
+#     # Jetstream
+#     js = nc.jetstream()
+
+#     # Pull based consumer on 'ais'.
+#     psub = await js.subscribe("ais", durable="fff")
+
+#     end_time = create_dump_file[0]
+#     start_time = create_dump_file[1]
+
+#     await get_messages(start_time, end_time, psub)
+
+#     file = f"dagster_ais/data/{format(str(end_time))}"
+#     # Close NATS connection
+#     await nc.close()
+
+#     return file
+
+@op
+def save_to_parquet(interval: tuple[datetime, datetime], messages: pd.DataFrame) -> str:
+    """Saves DataFrame to parquet and returns filepath"""
+    end_time, start_time = interval
+    file_path = f"dagster_ais/data/{format(str(end_time))}"
+    messages.to_parquet(file_path)
+    return file_path
+    
 @op(required_resource_keys={"minio_client"})
 def dump_file(context, file_name):
     minio_conn = context.resources.minio_client
@@ -41,20 +74,22 @@ async def append_to_file(message, time):
     file.close()
 
 async def get_messages(start_time, end_time, psub):
+    df = pd.DataFrame()
     while start_time < end_time:
         # # Get message
         msg = await psub.next_msg(20)
 
-        data = msg.data.decode()
+        data = json.loads(msg.data.decode())
         logger = get_dagster_logger()
         logger.info("DATA:")
         logger.info(data)
 
         # Extract the date
-        date = json.loads(data)["msgtime"]
+        date = data["msgtime"]
         timestamp = dateutil.parser.parse(date)
 
         # Append to file
+        df = df.append(data, ignore_index=True)
         await append_to_file(data, end_time)
 
         # # update the time
@@ -62,12 +97,9 @@ async def get_messages(start_time, end_time, psub):
 
         await msg.ack()
 
+    return df
 
-from dagster import op, get_dagster_logger
-from datetime import datetime, timedelta
-import pytz
-import os
-import dateutil.parser
+
 
 @op
 def create_dump_file():
@@ -89,3 +121,22 @@ def create_dump_file():
     logger.info(start_time)
 
     return now, start_time
+
+@op
+def get_interval() -> tuple[datetime, datetime]:
+    """
+    Gets the time interval to fetch data for 
+    """
+    utc = pytz.utc
+    # End time is the time the run is initiated
+    end_time = utc.localize(datetime.now())
+    # Start time defaults to yesterday
+    start_time = utc.localize(datetime.today() - timedelta(days=1))
+
+    logger = get_dagster_logger()
+    logger.info("end_time")
+    logger.info(end_time)
+    logger.info("start_time")
+    logger.info(start_time)
+
+    return end_time, start_time
