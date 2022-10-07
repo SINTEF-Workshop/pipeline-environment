1309f4cf8ffba08614a4e6060b1aa12e7a0732b7
erikjny <44464764+erikjny@users.noreply.github.com>
2022-10-07 09:43:34 +0000

    Added trajectory model job to airflow

--- test_airflow/dags/trajectory_model.py
+++ test_airflow/dags/trajectory_model.py
@@ -3,9 +3,13 @@ import dateutil.parser
 import pendulum
 import nats
 import os
+import mlflow
+import pytorch_lightning as pl
+import torch
 import pytz
 from datetime import timedelta, datetime
 import asyncio
+from ML_data_pipeline.src.simple_model.rnn_pl_model import RNNDataModule, RNN
 from airflow.decorators import dag, task
 from minio_conn import MinioClient
 
@@ -27,6 +31,50 @@ def trajectory_model():
         """
         pass
 
-    test = create_dump_file()
+    @task
+    def train_model():
+        batch_size = 5000
+        input_len = 10
+        hid_dim = 100
+        lr = 0.0001
+        output_len = 1
+        max_epochs = 10
+        patience = 10
+
+        # Data
+        dm = RNNDataModule(batch_size=batch_size, schema="ais", tablename='trajectories', input_len=input_len, output_len=output_len)
+
+        # Model
+        model = RNN(feature_dim=dm.feature_dim, hid_dim=hid_dim, lr=lr)
+
+        model.scaler = dm.scaler
+        model.train_idx = dm.train_idx
+        model.val_idx = dm.val_idx
+        model.test_idx = dm.test_idx
+
+        #mlflow.pytorch.autolog(registered_model_name=model_name)
+
+        with mlflow.start_run() as run:
+            mlflow.autolog()
+            mlflow.pytorch.log_model(pytorch_model=model, artifact_path='prediction_model')
+            mlflow.sklearn.log_model(dm.scaler, artifact_path='scaler')
+
+            # Training
+            early_stop_callback = pl.callbacks.early_stopping.EarlyStopping(monitor="val_loss", patience=patience,
+                                                                            mode="min")
+            #trainer = pl.Trainer(max_epochs=max_epochs, callbacks=[early_stop_callback], fast_dev_run=False)
+
+            # If using GPU, train on 2 GPUs, using the DDP strategy
+
+            trainer = pl.Trainer(max_epochs=max_epochs, #log_every_n_steps=1,
+                                callbacks=[early_stop_callback], fast_dev_run=False)
+
+            trainer.fit(model, dm)
+
+
+            print(model)
+            #torch.save(model,'../prelim_results/models/rnn_models/rnn_100.pt')
+
+    trained_model = train_model()
 
 trajectory_model = trajectory_model() 
